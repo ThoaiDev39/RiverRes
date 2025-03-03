da229288fe998af325090384c1ca21e684dc3457
ThoaiDev39 <31.lethoai.9.10@gmail.com>
2025-03-03 14:22:07 +0700

    update logic register and merge code

--- RiverRes/src/models/index.js
+++ RiverRes/src/models/index.js
@@ -4,14 +4,15 @@ const Dish = require('./dish');
 const Menu = require('./menu');
 const Hall = require('./hall');
 const Booking = require('./booking');
+const User = require('./Users');
 
 const syncDatabase = async () => {
     try {
-        await  sequelize.sync({ force: true, alter: true});  // Sử dụng alter để cập nhật bảng mà không xóa dữ liệu
+        await  sequelize.sync({ alter: true});  // Sử dụng alter để cập nhật bảng mà không xóa dữ liệu
         console.log('✅ Database đã được đồng bộ');
     } catch (error) {
         console.error('❌ Lỗi đồng bộ database:', error);
     }
 };
 
-module.exports = { syncDatabase , Dish, Menu, Hall, Booking };
+module.exports = { syncDatabase , Dish, Menu, Hall, Booking, User };
